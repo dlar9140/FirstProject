ffe408beaba797f44538d1c6e0b2298fe23e4440
David <davidlarson46@yahoo.com>
2019-01-22 09:02:05 -0700

    Initial commit

---
+++ FirstProject/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  FirstC++Project
+//
+//  Created by Larson, David on 1/22/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    // insert code here...
+    std::cout << "First time with C++\n";
+    
+}
